bf3acada4efb738c2c8e6c885952ab70bade1c78
Ashirbad Subudhi <ashirbad5subudhi@gmail.com>
2020-04-15 11:23:13 +0530

    Update Q1.js

--- JS INTERVIEW 1/Q1.js
+++ JS INTERVIEW 1/Q1.js
@@ -2,15 +2,17 @@
 // The functionality should be exactly the same as that of JS native .map() function. 
 // Function customMap() should return a new array and not change the array on which we are mapping
 
-Array.prototype.customMap = function() {
+Array.prototype.customMap = function(callback) {
     let ans = [];
     for(let i=0; i<this.length; ++i){
-        ans.push(this[i]*this[i]);
+        ans.push(callback(this[i], i, this));
     }
     return ans;
 }
 
-let arr = [1,2,3];
-let result = arr.customMap();
-console.log(result);
-console.log(arr);
+let arr = [1, 2, 3];
+let squaredNums = arr.customMap((item, index) => {
+	return item * item;
+}); 
+console.log(squaredNums); // should output [1, 4, 9]
+console.log(arr); // should output [1, 2, 3]
